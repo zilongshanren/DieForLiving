d6cb0b0f5b23d8a1871bfaf27a6d6e5ac824c875
zilongshanren <guanghui8827@126.com>
2024-07-06 10:15:36 +0800

    add more levels

--- scripts/game_manager.gd
+++ scripts/game_manager.gd
@@ -5,6 +5,12 @@ const LEVELS = [
 	"level_2",
 	"level_3",
 	"level_4",
+	"level_5",
+	"level_6",
+	"level_7",
+	"level_8",
+	"level_9",
+	"level_10",
 ]
 var score = 0
 var current_level = 0
@@ -18,15 +24,23 @@ func add_point():
 	score += 1
 	score_label.text = "You collected " + str(score) + " coins."
 
+func get_level_name(index):
+	return str("res://levels/", LEVELS[index], ".tscn")
+
 func get_level(index: int):
-	return load(str("res://levels/", LEVELS[index], ".tscn"))
+	return load(get_level_name(index))
 
 func go_to_next_level():
+
 	current_level +=1
+	var last_level = get_node("/root/Level" + str(current_level))
+	last_level.queue_free()
+
 	var level = get_level(current_level)
 	load_level(level)
 
 func load_level(level):
+
 	# Instance the new scene.
 	var current_scene = level.instantiate()
 
--- scripts/victory_point.gd
+++ scripts/victory_point.gd
@@ -3,5 +3,5 @@ extends Area2D
 
 
 func _on_body_entered(_body:Node2D) -> void:
-    var game_manager = get_node("/root/Game/GameManager") 
+    var game_manager = get_node("/root/Game/GameManager")
     game_manager.go_to_next_level()
