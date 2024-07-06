d24fd203c6f21bfd2f4035ff537be9c6cf099d87
baldSun <58137198+baldSun@users.noreply.github.com>
2024-07-06 11:23:43 +0800

    添加死亡body

---
+++ scripts/BodyMgr.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+@export var body_scene:PackedScene
+var last_body_create_timestamp = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	#add_to_group("bodyCaring")
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+func on_player_dead(player):
+
+	var body = body_scene.instantiate()
+
+	# Choose a random location on Path2D.
+	body.position = player.position
+
+	# Spawn the mob by adding it to the Main scene.
+	add_child(body)
--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -1,12 +1,23 @@
 extends Area2D
 
 @onready var timer = $Timer
+var last_body_create_timestamp = 0
 
 func _on_body_entered(body):
+	# 0.5s内只死亡一次
+	var cur_timestamp = Time.get_ticks_msec()
+	if cur_timestamp - last_body_create_timestamp < 500:
+		return
+	last_body_create_timestamp = cur_timestamp
+		
 	print("You died!")
-	Engine.time_scale = 0.5
-	body.get_node("CollisionShape2D").queue_free()
-	timer.start()
+	#var group_mem = get_tree().get_nodes_in_group("bodyCaring")
+	get_tree().call_group("bodyCaring", "on_player_dead", body)
+	#var group_node = get_tree().get_nodes_in_group("bodyCaring")
+	#body.visible = false
+	#Engine.time_scale = 0.5
+	#body.get_node("CollisionShape2D").queue_free()
+	#timer.start()
 
 
 func _on_timer_timeout():
