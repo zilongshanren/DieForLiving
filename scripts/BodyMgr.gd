e2e8b223a0ca1394e2b49b1ec1c19b6126ebf487
baldSun <58137198+baldSun@users.noreply.github.com>
2024-07-06 16:01:59 +0800

    修复多个尸体问题

--- scripts/BodyMgr.gd
+++ scripts/BodyMgr.gd
@@ -9,6 +9,12 @@ func _ready() -> void:
 	game_manager = get_node("/root/Game/GameManager")
 
 func on_player_dead(_player, pos):
+	# 0.5s内只死亡一次
+	var cur_timestamp = Time.get_ticks_msec()
+	if cur_timestamp - last_body_create_timestamp < 500:
+		return
+	last_body_create_timestamp = cur_timestamp
+	
 	var body = body_scene.instantiate()
 
 	# Choose a random location on Path2D.
