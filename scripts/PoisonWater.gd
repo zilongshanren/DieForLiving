baa23d803fb626e833179b9d645bd6d4890a09f7
maohuaxi <maohuaxi@tencent.com>
2024-07-06 15:46:07 +0800

    毒水

---
+++ scripts/PoisonWater.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var can_die = true
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var timer = $Timer
+	timer.timeout.connect(_on_DeathCooldownTimer_timeout)
+
+func _on_PoisonWater_body_entered(body):
+	if can_die:
+		get_tree().call_group("playerDeathCaring", "on_player_dead", body, body.global_position)
+		position.y -= 10
+		$Timer.start()
+		can_die = false
+
+func _on_DeathCooldownTimer_timeout():
+	can_die = true
